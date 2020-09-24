bb0aa18f119a9b4a0ba63fe9bd2f04a087c4be83
AndrejsGit <71761478+AndrejsGit@users.noreply.github.com>
2020-09-24 21:28:03 +0200

    static Background
    Animation noch nicht vorhanden, Background Material hinzugefügt.

---
+++ Assets/Scripts/BG_Scale.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class BG_Scale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //ermitteln der Höhe der Spielansicht. "Camera.main.orthographicsize" ermittelt die Hälfte der Höhe, daher *2
+        float height = Camera.main.orthographicSize * 2f;
+
+        //ermitteln der Breite des Bildes
+        float width = height * Screen.width/ Screen.height;
+
+        transform.localScale = new Vector3(width, height, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
