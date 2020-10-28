779ab1c08d95cab19b843e2d41d0a98ad30af86b
AndrejsGit <71761478+AndrejsGit@users.noreply.github.com>
2020-10-28 12:50:15 +0100

    BG Animation

---
+++ Assets/Scripts/BG_Animation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class BG_Animation : MonoBehaviour
+{
+
+    public float animationspeed = 0.1f; //Geschwindigkeit, mit der sich der Hintergrund bewegt 
+    private Material BGmaterial; //material = Objekte, z.B. Background
+    private Vector2 offset = Vector2.zero; //Offset/Verschiebung: zu Beginn auf Null gesetzt
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGmaterial = GetComponent<Renderer>().material; //auslesen des Materials (z.B. Background) aus dem Renderer
+        offset = BGmaterial.GetTextureOffset("_MainTex"); //Textureoffset getten, "_MainTex" beschreibt Eigenschaften des Bildes bei Speicherung. Des Weiteren gibt es Blendtex etc.
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.x = offset.x + animationspeed * Time.deltaTime; //Time.deltatime = Vergangene Zeit bis zur vollständigen Anzeige des letzten Frames
+        BGmaterial.SetTextureOffset("_MainTex", offset); //Material bekommt Offset hinzugefügt, offset wird frame für frame größer
+
+    }
+}
