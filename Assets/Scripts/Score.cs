4692a9c9b5eb5ecd42802c676866e26de95acac3
AndrejsGit <71761478+AndrejsGit@users.noreply.github.com>
2020-11-22 18:17:11 +0100

    Score und Highscore Anzeige

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Zugriff auf UI's möglich
+
+public class Score : MonoBehaviour
+{
+    
+    public Text aktueller_Score;
+    public Text Highscore;
+    
+    public float ScoreCount;
+    public float HighScoreCount;
+
+    public float PointsPerSecond;
+
+    public bool PlayerAlive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerAlive = true;
+
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            HighScoreCount = PlayerPrefs.GetFloat("HighScore"); //HighScore-Anzeige steht bei HighScoreWert und nicht bei 0.
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerAlive)
+        {
+            ScoreCount += PointsPerSecond * Time.deltaTime; //Score wird hochgezählt, wenn Spieler noch lebt
+        }
+
+        if (ScoreCount >= HighScoreCount) 
+        {
+            HighScoreCount = ScoreCount;  //Highscore nimmt Wert von Score an, wenn Score den Highscore überschreitet
+            PlayerPrefs.SetFloat("HighScore", HighScoreCount); //HighScore wird gespeichert
+        }
+        
+        
+
+        aktueller_Score.text = "Score: " + Mathf.Round(ScoreCount);   //Score wird geprintet
+        Highscore.text = "HighScore: " + Mathf.Round(HighScoreCount); //HighScore wird geprintet
+    }
+
+   
+}
