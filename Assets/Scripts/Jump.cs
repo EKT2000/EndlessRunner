7de45e1c318347c0ba58fe53ba457686f80def19
RobinMM1 <robin-dietzel@hotmail.com>
2020-11-25 13:56:26 +0100

    added jumpscript & character

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float JumpForce = 2;
+    private Rigidbody2D _rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
