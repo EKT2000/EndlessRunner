a5fc79f5b5e60465abe7ef2c0c26e993f0575dc0
RobinMM1 <robin-dietzel@hotmail.com>
2020-12-06 20:57:32 +0100

    added ObstaceSpawner and GameOver-Script

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.tag == "Collector") {
+            RestartGame();
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        
+        if (other.gameObject.tag == "Enemy") {
+            RestartGame();
+        }
+
+    }
+
+    void RestartGame() {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+}
--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -7,10 +7,16 @@ public class ObstacleSpawner : MonoBehaviour
     public GameObject[] obstacles;
     public List<GameObject> obstaclesToSpawn = new List<GameObject>();
 
+    int index;
+
+    void Awake() {
+        InitObstacles ();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnRandomObstacle());
     }
 
     // Update is called once per frame
@@ -19,8 +25,40 @@ public class ObstacleSpawner : MonoBehaviour
         
     }
 
-    void Initobstacles()
+    void InitObstacles()
     {
+        index = 0;
+        //Initialisere Hindernisse 
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            GameObject obj = Instantiate (obstacles [index], transform.position, Quaternion.identity);
+            obstaclesToSpawn.Add(obj);
+            index++;
+            obstaclesToSpawn[i].SetActive(false);
+            if(index == obstacles.Length) {
+                index = 0;
+            }
+        }
+    }
 
+    IEnumerator SpawnRandomObstacle()
+    {
+        // Warte eine gewisse Zeit
+        yield return new WaitForSeconds (Random.Range (1.5f, 4.5f));
+        int index = Random.Range(0, obstaclesToSpawn.Count);
+
+        while(true)
+        {
+            if(!obstaclesToSpawn[index].activeInHierarchy) {
+                obstaclesToSpawn [index].SetActive(true);
+                obstaclesToSpawn [index].transform.position = transform.position;
+                break;
+            }
+            else {
+                index = Random.Range(0, obstaclesToSpawn.Count);
+            }
+        }
+
+        StartCoroutine(SpawnRandomObstacle());
     }
 }
